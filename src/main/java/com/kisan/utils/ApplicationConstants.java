b52ef3f2d079710bf85a5f95d9b12a544bc7b827
janakiramulu464 <reachjanakiramulu@gmail.com>
2018-10-11 00:24:23 +0530

    update ui project with cutomer screen

--- src/main/java/com/kisan/utils/ApplicationConstants.java
+++ src/main/java/com/kisan/utils/ApplicationConstants.java
@@ -1,7 +1,7 @@
 package com.kisan.utils;
 
 public class ApplicationConstants {
-    public final static int SUCCESS_CODE = 0;
-    public final static int ERROR_CODE = -1;
+    public final static int SUCCESS_CODE = 200;
+    public final static int ERROR_CODE = 400;
 
 }
